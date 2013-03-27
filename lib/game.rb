7078ffe229a927cfe663eb64be165bd089dc0361
Steve+India <steve-india@appacademy.io>
2013-03-26 18:29:53 -0700

    complete part 1

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,14 @@
 class Game
 
-	# def place_piece(position, color)
-	# end
+	attr_accessor :board
+
+	def initialize(board)
+		self.board = board ||= Board.new
+	end
+
+	def place_piece(position, color)
+		board.place(color, position)
+	end
+
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,22 +7,22 @@ describe Game do
 	describe "#initialize" do
 
 		it "it initializes the game" do
+
 			board = double('board')
-			board.should_receive(:new)
-			game = Game.new
+			game = Game.new(board)
+			game.board.should be_true
 		end
 
 	end
 
 	describe "#place_piece" do
-
-		let(:game) { Game.new }
+		let(:board) { double('board') }
+		subject(:game) { Game.new(board) }
 
 		it "tells the board to place a piece" do
 			position = [0,0]
 			color = :red
-			board = double('board')
-			board.should_receive(:place).with(position, color)
+			game.board.should_receive(:place).with(color, position)
 			game.place_piece(position, color)
 		end
 
